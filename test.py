6a8b5b0738235ee75611d23fec77adac5251046b
Zeraye <jakubrudnik02@gmail.com>
2021-11-18 09:45:00 -0500

    Removed unnecessary file

--- test.py
+++
@@ -1,7 +0,0 @@
-import re
-def num_from_val(value):
-    return int("".join(re.findall("[-\d]", value)))
-
-print(type(num_from_val("INTEGER(-35)")))
-print(num_from_val("35"))
-print(num_from_val("3a5"))
